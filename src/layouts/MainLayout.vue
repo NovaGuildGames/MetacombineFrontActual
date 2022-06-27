e1fc71fe96b475af692dc3f2a555c6b02ec2cc43
Dettroxx <Roxxdett@gmail.com>
2022-06-27 18:07:23 +0300

    base footer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,34 +1,76 @@
 <template>
-  <q-layout view="hHh Lpr fFf">
+  <q-layout view="hHh lpR lfr">
 
-    <q-header elevated>
+    <q-header reveal class="bg-white text-black">
       <q-toolbar>
         <q-toolbar-title>
-          Header
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-black.svg">
+          </q-avatar>
+          <b>Meta</b>Combine
         </q-toolbar-title>
+
+        <q-btn flat no-wrap>
+            <q-avatar rounded size="38px">
+              <img src="https://cdn.quasar.dev/img/avatar3.jpg">
+            </q-avatar>
+            <q-icon name="arrow_drop_down" size="16px" />
+
+            <q-menu auto-close>
+              <q-list dense>
+                <q-item class="GL__menu-link-signed-in">
+                  <q-item-section>
+                    <div>Signed in as <strong>Mary</strong></div>
+                  </q-item-section>
+                </q-item>
+                <q-separator />
+                <q-item clickable class="GL__menu-link-status">
+                  <q-item-section>
+                    <div>
+                      <q-icon name="tag_faces" color="blue-9" size="18px" />
+                      Set your status
+                    </div>
+                  </q-item-section>
+                </q-item>
+                <q-separator />
+                <q-item clickable class="GL__menu-link">
+                  <q-item-section>Your profile</q-item-section>
+                </q-item>
+                <q-item clickable class="GL__menu-link">
+                  <q-item-section>Your repositories</q-item-section>
+                </q-item>
+                <q-item clickable class="GL__menu-link">
+                  <q-item-section>Your projects</q-item-section>
+                </q-item>
+                <q-item clickable class="GL__menu-link">
+                  <q-item-section>Your stars</q-item-section>
+                </q-item>
+                <q-item clickable class="GL__menu-link">
+                  <q-item-section>Your gists</q-item-section>
+                </q-item>
+                <q-separator />
+                <q-item clickable class="GL__menu-link">
+                  <q-item-section>Help</q-item-section>
+                </q-item>
+                <q-item clickable class="GL__menu-link">
+                  <q-item-section>Settings</q-item-section>
+                </q-item>
+                <q-item clickable class="GL__menu-link">
+                  <q-item-section>Sign out</q-item-section>
+                </q-item>
+              </q-list>
+            </q-menu>
+          </q-btn>
       </q-toolbar>
     </q-header>
-
-    <q-footer>
-      <q-tabs switch-indicator>
-        <q-route-tab
-          icon="map"
-          to="/your/route"
-          replace
-          label="One Tab"
-        />
-        <q-route-tab
-          icon="assignment"
-          to="/some/other/route"
-          replace
-          label="Other Tab"
-        />
-      </q-tabs>
-    </q-footer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-footer class="bg-white text-black">
+      Footer
+    </q-footer>
 
   </q-layout>
 </template>
